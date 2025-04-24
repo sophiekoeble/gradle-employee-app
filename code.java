c61c19165db85f7041017d6e0e10ef7f60449095
Sophie Maria Koble <s.koeble@gmx.net>
2025-04-24 16:31:33 +0200

    add second class

--- code.java
+++ code.java
@@ -3,3 +3,18 @@ public class Employee {
     String emailAddress;
     int yearOfBirth;
 }
+
+
+public class EmployeeApp {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+
+        employee.name = "John";
+        employee.emailAddress = "john@baeldung.com";
+        employee.yearOfBirth = 1978;
+
+        System.out.println("Name: " + employee.name);
+        System.out.println("Email Address: " + employee.emailAddress);
+        System.out.println("Year of Birth: " + employee.yearOfBirth);
+    }
+}
