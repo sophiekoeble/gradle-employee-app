a38e6c56c9720cc5c9aa6bfe9c0c312be79188b8
Sophie Maria Koble <s.koeble@gmx.net>
2025-04-30 17:20:13 +0200

    add PersonalData Test

---
+++ src/test/java/employee/PersonalDataTest.java
@@ -0,0 +1,28 @@
+package employee;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+ 
+/**
+ *
+ * @author skoble
+ */
+public class PersonalDataTest {
+
+    @Test
+    public void testLoadEmployeesFromCSV() {
+        PersonalData personalData = new PersonalData();
+        personalData.loadEmployeesFromCSV();
+
+        assertEquals(4, personalData.getEmployees().size());
+        for (Employee testEmployee : personalData.getEmployees()) {
+            assertNotNull(testEmployee);
+            assertNotNull(testEmployee.getName());
+            assertNotNull(testEmployee.getEmailAddress());
+            assertTrue(testEmployee.getYearOfBirth() > 1900);
+        }
+    }
+
+}
