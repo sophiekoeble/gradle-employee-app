ab1991715ffed83ae5279f206933dc1cce3e7c6e
Sophie Maria Koble <s.koeble@gmx.net>
2025-04-30 16:35:13 +0200

    remove spring context

--- src/main/java/employee/EmployeeApp.java
+++ src/main/java/employee/EmployeeApp.java
@@ -1,8 +1,5 @@
 package employee;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class EmployeeApp {
 
     public static void main(String[] args) {
